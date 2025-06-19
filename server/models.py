a24cab061701b677bb986672e401472b06746b94
DenisMaiyo <denismaiyo14@gmail.com>
2025-06-20 00:27:13 +0300

    finished

--- server/models.py
+++ server/models.py
@@ -7,7 +7,9 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 
-class Pet(db.Model):
+from sqlalchemy_serializer import SerializerMixin
+
+class Pet(db.Model, SerializerMixin):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
